e3a5da514c8aac63dcc98a9c10f69a5336c6106a
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-10-24 07:20:23 +0530

    request maps

--- Spring-Boot-CRUD/src/main/java/com/example/SpringBootCRUD/controller/UserController.java
+++ Spring-Boot-CRUD/src/main/java/com/example/SpringBootCRUD/controller/UserController.java
@@ -1,17 +1,29 @@
 package com.example.SpringBootCRUD.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
 
 public class UserController {
-    @GetMapping("/getUer")
-    public String getUser(){
+    @GetMapping("/getUser")
+    public String getUser() {
         return "Simple-Root";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser() {
+        return "User Saved";
+    }
+
+    @PutMapping("/updateUser")
+    public String updateUser() {
+        return "User Updated";
+    }
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser() {
+        return "User Deleted";
     }
 }
